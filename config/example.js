b37fee297aa57f3384a28a3bb23fc78b43fb187f
Marcus Hultman <marcus.hultman1@gmail.com>
2020-02-06 19:35:49 +0100

    add example config

---
+++ config/example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  server: 'www.for.email.addresses',
+  db: {
+    url : `database`
+  },
+  mail: {
+    url: `smtps://email`
+  },
+  instagram: {
+    token: 'api'
+  },
+  secret: 'secret'
+}
